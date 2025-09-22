5f75bdce6aba57c4a0e53ccf46a4623e987fafb7
manoelsouzadev <manoel.souza280@gmail.com>
2025-09-22 09:46:42 -0300

    Commit inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'pages/home_page.dart';
 
-void main() {
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io' show Platform;
+
+Future<void> _initDbFactory() async {
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  } else {
+    try {
+      if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+        sqfliteFfiInit();
+        databaseFactory = databaseFactoryFfi;
+      }
+    } catch (_) {
+      // Se Platform não estiver disponível por algum motivo, ignore.
+    }
+  }
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await _initDbFactory();
   runApp(const BibliotecaApp());
 }
 
--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,50 +1,46 @@
 class Book {
-final int? id;
-final String titulo;
-final String autor;
-final int ano;
-final String genero;
-final int paginas;
+  final int? id;
+  final String titulo;
+  final String autor;
+  final int ano;
+  final String genero;
+  final int paginas;
 
+  Book({
+    this.id,
+    required this.titulo,
+    required this.autor,
+    required this.ano,
+    required this.genero,
+    required this.paginas,
+  });
 
-Book({
-this.id,
-required this.titulo,
-required this.autor,
-required this.ano,
-required this.genero,
-required this.paginas,
-});
+  Book copyWith({int? id, String? titulo, String? autor, int? ano, String? genero, int? paginas}) {
+    return Book(
+      id: id ?? this.id,
+      titulo: titulo ?? this.titulo,
+      autor: autor ?? this.autor,
+      ano: ano ?? this.ano,
+      genero: genero ?? this.genero,
+      paginas: paginas ?? this.paginas,
+    );
+  }
 
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'titulo': titulo,
+        'autor': autor,
+        'ano': ano,
+        'genero': genero,
+        'paginas': paginas,
+      };
 
-Book copyWith({int? id, String? titulo, String? autor, int? ano, String? genero, int? paginas}) {
-return Book(
-id: id ?? this.id,
-titulo: titulo ?? this.titulo,
-autor: autor ?? this.autor,
-ano: ano ?? this.ano,
-genero: genero ?? this.genero,
-paginas: paginas ?? this.paginas,
-);
-}
-
-
-Map<String, dynamic> toMap() => {
-'id': id,
-'titulo': titulo,
-'autor': autor,
-'ano': ano,
-'genero': genero,
-'paginas': paginas,
-};
-
-
-factory Book.fromMap(Map<String, dynamic> map) => Book(
-id: map['id'] as int?,
-titulo: map['titulo'] as String,
-autor: map['autor'] as String,
-ano: (map['ano'] as num).toInt(),
-genero: map['genero'] as String,
-paginas: (map['paginas'] as num).toInt(),
-);
+  factory Book.fromMap(Map<String, dynamic> map) => Book(
+        id: map['id'] as int?,
+        titulo: map['titulo'] as String,
+        autor: map['autor'] as String,
+        ano: (map['ano'] as num).toInt(),
+        genero: map['genero'] as String,
+        paginas: (map['paginas'] as num).toInt(),
+      );
 }
